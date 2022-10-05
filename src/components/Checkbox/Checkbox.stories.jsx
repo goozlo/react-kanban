4b51b0fbf0c3b2e096de31b2b111c1349b89984c
Nikita <nikitaleonov00@mail.ru>
2022-10-05 21:06:51 +0300

    changed package

--- src/components/Checkbox/Checkbox.stories.jsx
+++ src/components/Checkbox/Checkbox.stories.jsx
@@ -4,16 +4,16 @@ import {Checkbox} from './'
 export default {
     title: 'Checkbox',
     component: Checkbox,
-    // argTypes: {
-    //     label: {
-    //         description: 'Строка, описывающая соглашение на которое влияет checkbox',
-    //         defaultValue: 'label wasn\'t provided'
-    //     },
-    //     mode: {
-    //         description: 'Тема внешнего вида приложения',
-    //         defaultValue: 'dark'
-    //     }
-    // }
+    argTypes: {
+        label: {
+            description: 'Строка, описывающая соглашение на которое влияет checkbox',
+            defaultValue: 'label wasn\'t provided'
+        },
+        mode: {
+            description: 'Тема внешнего вида приложения',
+            defaultValue: 'dark'
+        }
+    }
 }
 
 const Template = arg => <Checkbox {...arg}/>
