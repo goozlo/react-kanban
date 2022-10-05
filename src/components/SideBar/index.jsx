7def6fcdc9a1ae650ff4471b04ba079f60dd212c
Vlad <belousov7vlad@gmail.com>
2022-10-06 00:05:40 +0300

    bordsList el add

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ThemeSwiper from '../ThemeSwiper/index';
 import { Link } from "react-router-dom";
 import './sideBar.scss'
+import BordsList from './BordsList';
 
 const SideBar = () => {
 
@@ -10,16 +11,8 @@ const SideBar = () => {
 
     <div className="sidebar">
       <div className="sidebar__container">
-        <div className='sidebar__nav'>
-          <h2 className=''>ALL BOARDS (3)</h2>
-          <div className='sidebar__links'>
-            <Link className='sidebar__link'> Доска 1</Link>
-            <Link className='sidebar__link'> Доска 2</Link>
-          </div>
-        </div>
-
+        <BordsList />
         <ThemeSwiper />
-
       </div>
 
     </div>
