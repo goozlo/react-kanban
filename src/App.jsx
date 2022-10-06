4c889c95eb1a9e2214859dae4e4f7a61c8c029af
Nikita <nikitaleonov00@mail.ru>
2022-10-06 15:38:14 +0300

    fixed errors

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import {Routes, Route} from 'react-router-dom'
 import MainPage from './components/MainPage/MainPage';
 import {Modal} from "./components/Modal";
 import './styles/@global.scss'
+import {Checkbox} from "./components/Checkbox";
 
 const App = () => {
 
 
     return (
         <>
+            <Checkbox/>
             <Modal/>
             <Routes>
                 <Route path="/" element={<MainPage/>}/>
