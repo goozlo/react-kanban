66da0783d5237243c54e7b1692190b8b9cfdd470
Nikita <nikitaleonov00@mail.ru>
2022-10-06 13:51:56 +0300

    added swiper

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { Routes, Route } from 'react-router-dom'
-import './styles/@global.scss'
+import {Routes, Route} from 'react-router-dom'
 import MainPage from './components/MainPage/MainPage';
 import {Checkbox} from "./components/Checkbox";
+import './styles/@global.scss'
+
 
 const App = () => (
     <>
