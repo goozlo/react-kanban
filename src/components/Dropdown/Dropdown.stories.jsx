8316eb76bd46c148e1afcdd5bec5a422e2974f4b
Nikita <nikitaleonov00@mail.ru>
2022-10-07 20:59:23 +0300

    added storybook for dropdown

--- src/components/Dropdown/Dropdown.stories.jsx
+++ src/components/Dropdown/Dropdown.stories.jsx
@@ -1,22 +1,44 @@
 import React from "react";
 import {Dropdown} from './'
+import {Provider} from "react-redux";
+import store from "../../store";
 
 export default {
     title: 'Dropdown',
     component: Dropdown,
-    // argTypes: {
-    //     label: {
-    //         description: 'Строка, описывающая соглашение на которое влияет checkbox',
-    //     }
-    // }
+    argTypes: {
+        data: {
+            description: '...',
+        }
+    }
 }
 
 const Template = args => <Dropdown {...args}/>
 
 export const Primary = Template.bind({})
-// Primary.args = {
-//     label: 'label wasn\'t provided'
-// }
+Primary.args = {
+    data: {
+        label: 'Current Status',
+        options: [
+            {
+                id: 0,
+                label: 'Todo',
+                value: 'Todo'
+            },
+            {
+                id: 1,
+                label: 'Doing',
+                value: 'Doing'
+            },
+            {
+                id: 2,
+                label: 'Done',
+                value: 'Done'
+            }
+        ]
+    }
+}
+
 
 
 
