d0374b5dc6548f3d8dd35790d2caf44859843c78
Ruslan Gaziev <grinrus@yandex.ru>
2022-10-12 01:20:53 +0300

    board added, sidebar some code changed

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import './MainPage.scss';
 import SideBar from '@components/SideBar/index';
 import Header from '@components/Header/index';
+import Board from '@components/Board/index';
 
 function MainPage() {
     return (
-        <>
-            <Header/>
-            <SideBar/>
-        </>
+        <main className='main-page'>
+            <Header />
+            <div className='main-page__container'>
+                <SideBar />
+                <Board />
+            </div>
+        </main>
     );
 }
 
--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -5,12 +5,17 @@ import "./sideBar.scss";
 import BordsList from "./BordsList";
 
 const SideBar = () => {
+
   return (
     <>
       <div className="sidebar">
         <div className="sidebar__container">
           <BordsList />
           <Toggle />
+          <button className='sidebar__hide-btn' type='button'>
+            <div className='sidebar__hide-icon'></div>
+            <span className='sidebar__hide-title'>Hide Sidebar</span>
+          </button>
         </div>
       </div>
     </>
