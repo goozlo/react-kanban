8548cd4851d32abeda13d78c18f9ebc9cd3d3d28
Polina <kaminskaia.p.a@gmail.com>
2022-10-12 12:27:13 +0300

    remove button from main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import { Modal } from '@components/Modal';
-import Button from '@components/Button';
 import MainPage from './components/MainPage/MainPage';
-// import TaskColumn from '@components/TaskColumn';
 import './styles/@global.scss';
 
 function App() {
   return (
     <>
       <Modal />
-      <Button />
       <Routes>
         <Route path="/" element={<MainPage />} />
       </Routes>
