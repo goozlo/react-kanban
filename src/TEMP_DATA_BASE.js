f3a7f1bfd01fd34b2801c1ccb0fe5931743a4fdb
Vlad <belousov7vlad@gmail.com>
2022-10-14 20:41:37 +0300

    db test

--- src/TEMP_DATA_BASE.js
+++ src/TEMP_DATA_BASE.js
@@ -2,17 +2,36 @@ export let bords = [
     {
         bord_id: 0,
         bordName: "Доска 1",
-        tasks:
-            [{
-                task_id: 0,
-                title: 'Build UI for boarding flow',
-                subtasks: '0 of 3 subtasks'
-            },
-            {
-                task_id: 1,
-                title: 'Build UI for search',
-                subtasks: '0 of 1 subtasks'
-            },]
+        columns: [{
+            toDo: [{
+                tasks:
+                    [{
+                        task_id: 0,
+                        title: 'Build UI for boarding flow',
+                        subtasks: '0 of 3 subtasks'
+                    },
+                    {
+                        task_id: 1,
+                        title: 'Build UI for search',
+                        subtasks: '0 of 1 subtasks'
+                    }]
+            }],
+            done: [{
+                tasks:
+                    [{
+                        task_id: 0,
+                        title: 'Build UI for boarding flow',
+                        subtasks: '0 of 3 subtasks'
+                    },
+                    {
+                        task_id: 1,
+                        title: 'Build UI for search',
+                        subtasks: '0 of 1 subtasks'
+                    }]
+            }],
+        }
+        ],
+
     },
     {
         bord_id: 1,
