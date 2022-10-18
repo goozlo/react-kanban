de66daeb1d3e3996aa46117f1c107181295ebfcd
Ruslan Gaziev <grinrus@yandex.ru>
2022-10-19 01:36:02 +0300

    columns and tasks added

--- src/components/Task/index.jsx
+++ src/components/Task/index.jsx
@@ -1,13 +1,23 @@
+import { useEffect, useState } from 'react';
 import './task.scss';
 
-const Task = ({data}) => {
+const Task = ({ data }) => {
     // по айди понятно какую таску взяли/тыкнули/переносят и тд
-    const {taskContent = 'Build UI for boarding flow', checklist = '3 of 3 subtasks', id} = data
+    const {title, description, status, subtasks, checklist = '3 of 3 subtasks', id} = data;
+    const [complitedSubtasks, setComplitedSubtasks] = useState(0);
+    
+
+    // Считает количество выполненных subtasks
+    useEffect(() => {
+        let count = 0;
+        subtasks.forEach(subtask => subtask.isCompleted === true ? count = count + 1 : '');
+        setComplitedSubtasks(count)
+    }, [subtasks])
 
     return (
         <div className={"task"}>
-            <p className={"task__title"}>{taskContent}</p>
-            <p className={"task__subscribe"}>{checklist}</p>
+            <p className={"task__title"}>{title}</p>
+            <p className={"task__subscribe"}>{`${complitedSubtasks} of ${subtasks.length} subtasks`}</p>
         </div>
     );
 };
--- src/components/TaskColumn/index.jsx
+++ src/components/TaskColumn/index.jsx
@@ -1,33 +1,20 @@
+import { useState, useEffect } from 'react';
 import Task from "@components/Task";
+import './TaskColumn.scss';
 
-let tasksArray = [
-  {
-    id: 0,
-    title: 'Build UI for boarding flow',
-    subtasks: '0 of 3 subtasks'
-  },
-  {
-    id: 1,
-    title: 'Build UI for search',
-    subtasks: '0 of 1 subtasks'
-  },
-  {
-    id: 2,
-    title: 'Create template structures',
-    subtasks: '0 of 2 subtasks'
-  },
-  {
-    id: 3,
-    title: 'QA and test all major user journeys',
-    subtasks: '0 of 2 subtasks'
-  }
-]
+const TaskColumn = ({ column, tasks }) => {
+  const [properTasks, setProperTasks] = useState([]);
 
-const TaskColumn = () => {
+  // Колонка получает на вход массив со всеми тасками доски. Функция фильтрует таски для конкретной колонки
+  useEffect(() => {
+    const filteredTasks = tasks.filter(task => task.columnId === column.id);
+    setProperTasks(filteredTasks);
+  }, [column, tasks]);
+
   return (
-      <div>
-        <h3>doing (2/3)</h3>
-        {tasksArray.map(task => <Task key={task.id} data={task}/>)}
+      <div className='column'>
+        <h3>{`${column.name.toUpperCase()} ( ${properTasks.length} )`}</h3>
+        {properTasks.map(task => <Task key={task.id} data={task}/>)}
       </div>
   )
 }
